99f509f205077f19536dd9ec849b99ce8edacefa
Brian Sukhnandan <briansukhnandan@gmail.com>
2019-02-26 23:54:44 -0500

    Added more movement + collision

--- Club-Project/Assets/PlayerMovement.cs
+++ Club-Project/Assets/PlayerMovement.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-  //  public CharacterController2D controller;
     public float avgSpeed = 10f;
     public float horizontalMovement = 0f;
 
-    //Because of this game's nature, we don't need to jump or crouch.
-    bool jump = false;
-    bool crouch = false;
+    //A reference to the animator we made, so it's private, we don't want a duplicate showing up in Unity.
+    private Animator animationHandler;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        //Initializes the animationHandler to the animator attached to the object this script is attached to.
+        animationHandler = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -25,8 +27,11 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetAxisRaw("Horizontal") > 0.1f || Input.GetAxisRaw("Horizontal") < -0.1f)
         {
+
+
             //Will be positive or negative based on input, no need to add an extra if statement.
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * avgSpeed * Time.deltaTime, 0f, 0f));
+
 
         }
 
@@ -37,7 +42,11 @@ public class PlayerMovement : MonoBehaviour
 
         }
 
+        animationHandler.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
 
+        animationHandler.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+
+       
     }
 
    
